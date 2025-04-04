2529f7a64622072be90e859d2bddcb2d691a2658
JakubObrycki <128688775+JakubObrycki@users.noreply.github.com>
2025-04-04 11:57:02 +0200

    Update light_main.ino

--- light_main.ino
+++ light_main.ino
@@ -1,23 +1,22 @@
 #include <SD.h>     
 #include <SPI.h>
 #include <Arduino.h>
-#include <hp_BH1750.p>
+#include <hp_BH1750.h>
 
 hp_BH1750 sens1;
 hp_BH1750 sens2;
 
 unsigned int lux1, lux2;
-bool ready1, ready2;
-bool forceReading;
 File plik;
 
 void setup() {
-  
-  forceReading = True;
   Serial.begin(9600);
-  Serial.println("");
-  sens1.begin(BH1750_TO_VCC);
-  sens2.begin(BH1750_TO_GROUND);
+  Serial.println("Uruchomiono");
+
+  if (!sens1.begin(BH1750_TO_VCC) || !sens2.begin(BH1750_TO_GROUND)) {
+    Serial.println("Błąd inicjalizacji sensorów");
+    return;
+  }
 
   sens1.calibrateTiming();
   sens2.calibrateTiming();
@@ -25,37 +24,38 @@ void setup() {
   sens1.start(BH1750_QUALITY_HIGH2, BH1750_MTREG_DEFAULT);
   sens2.start(BH1750_QUALITY_HIGH2, BH1750_MTREG_DEFAULT);
 
-  while(!Serial){}
-Serial.println("Uruchomiono");
-if(!SD.begin(4)) {
-  Serial.println("Problem z inicjalizacja karty");
-  return;
-}
-  Serial.println("Zainicjalizowanie karte");
-  plik = SD.open("Pomiar_15_07.txt", FILE_WRITE);
+  if (!SD.begin(4)) {
+    Serial.println("Problem z inicjalizacją karty SD");
+    return;
+  }
 
-if(!plik)
-  {
+  Serial.println("Zainicjalizowano kartę SD");
+
+  plik = SD.open("Pomiar_15_07.txt", FILE_WRITE);
+  if (!plik) {
     Serial.println("Problem z otwarciem pliku");
+    return;
   }
 }
+
 void loop() {
   lux1 = sens1.getLux();
-  Serial.println("Light sensor 1: ");
-  Serial.println(lux1);
-  sens1.start();
-
   lux2 = sens2.getLux();
-  Serial.println("Light sensor 2: ");
+
+  Serial.print("Light sensor 1: ");
+  Serial.println(lux1);
+  
+  Serial.print("Light sensor 2: ");
   Serial.println(lux2);
-  sens1.start();
 
-  plik.println(lux1 = sens1.getLux());
-  plik.println(lux2 = sens2.getLux());
+  plik.print("Sensor 1: ");
+  plik.println(lux1);
+  plik.print("Sensor 2: ");
+  plik.println(lux2);
 
-  ready1 = sens1.hasValue(forceReading);
-  ready2 = sens2.hasValue(forceReading);
+  if (!plik) {
+    Serial.println("Błąd zapisu do pliku");
+  }
+
   delay(12000);
-
 }
-
